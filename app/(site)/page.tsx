8dc2e187bb81810ecd2aa5988f5ebb24b601c24c
Valerfa <fatykhova.v@gmail.com>
2026-05-20 14:28:34 +0300

    added description for search result

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Gallery from "@/components/Gallery";
@@ -5,6 +6,11 @@ import Work from "@/components/Work";
 import Clients from "@/components/Clients";
 import Footer from "@/components/Footer";
 import CTA from "@/components/CTA";
+
+export const metadata: Metadata = {
+  description:
+    "Озвучиваю частные и корпоративные ивенты, бизнес-мероприятия для ведущих компаний и брендов России, фестивали, конференции и дни города.",
+};
 
 export default function HomePage() {
   return (
